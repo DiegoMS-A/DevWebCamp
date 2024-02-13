c71c5dba2ad4380657599df4eb00d839b6a5dff2
DiegoMS-A <diegomasaar@gmail.com>
2024-02-13 13:12:51 +0100

    first commit

---
+++ src/js/mapa.js
@@ -0,0 +1,19 @@
+if(document.querySelector('#mapa')) {
+
+    const lat = 39.85799
+    const lng = -4.02023
+    const zoom = 16
+
+    const map = L.map('mapa').setView([lat, lng], zoom);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([lat, lng]).addTo(map)
+        .bindPopup(`
+            <h2 class="mapa__heading">DevWebCamp</h2>
+            <p class="mapa__texto">Alcazar de Toledo</p>
+        `)
+        .openPopup();
+}
